30b885b417c6979c69eccf69b2f90777e8d6d6c9
Lucky-victory <blizwonder25@gmail.com>
2022-04-04 10:38:06 +0100

    updated player component

--- ng-music-player/src/app/player/player.type.ts
+++ ng-music-player/src/app/player/player.type.ts
@@ -1,34 +1,32 @@
-
-
-export interface ISongList{
-    id:(string | number);
-    title:string;
-   artist:string;
-cover:string;
-url:string,
-favorite:boolean
+export interface ISongList {
+  id: string | number;
+  title: string;
+  artist: string;
+  cover: string;
+  url: string;
+  favorite: boolean;
 }
-export interface IPlayer{
-    songList:ISongList[];
-  isPlaying:boolean;
-  song:ISongList;
-   currentIndex:number;
-  isRepeatAll:boolean;
-isShuffle:boolean;
-repeatIcon:string;
- progressPercent:string;
-repeatState:RepeatState;
-totalDuration:string;
-timePlayed:string;
-    play:()=> void;
-    pause:()=>void;
-    playAndPause:()=>void;
-    next:()=>void;
-    previous:()=>void;
-    repeat:()=>void;
-    shuffle:()=>void;
-    onEnded:()=>void;
-    toggleFavorite:(evt:Event)=>void;
+export interface IPlayer {
+  songList: ISongList[];
+  isPlaying: boolean;
+  song: ISongList;
+  currentIndex: number;
+  isRepeatAll: boolean;
+  isShuffle: boolean;
+  repeatIcon: string;
+  progressPercent: string;
+  repeatState: RepeatState;
+  totalDuration: string;
+  timePlayed: string;
+  showPlayList(): void;
+  play: () => void;
+  pause: () => void;
+  playAndPause: () => void;
+  next: () => void;
+  previous: () => void;
+  repeat: () => void;
+  shuffle: () => void;
+  onEnded: () => void;
+  toggleFavorite: (evt: Event) => void;
 }
-export type RepeatState = 'repeat one'|'repeat all'|'repeat off';
-    
+export type RepeatState = 'repeat one' | 'repeat all' | 'repeat off';
